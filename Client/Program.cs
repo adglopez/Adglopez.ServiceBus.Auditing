63727684df497ad7243905cc6bcae00cd9f8f9c2
adglopez <adglopez@gmail.com>
2013-07-21 00:57:12 +1200

    Added Service Bus Explorer

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 using Adglopez.Samples.ServiceBus.Auditing.Client.AuditProxy;
